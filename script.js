7ca23fc9b7c9b19e2f0098176c7aac6cb17283a3
Bernardo Santos <91677263+BernvrdxS@users.noreply.github.com>
2022-02-15 13:19:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Cadastrar() {
+
+    var nome = document.getElementById("nome");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram cadastrados com sucesso');
+    }
+
+}
